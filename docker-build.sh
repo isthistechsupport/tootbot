e75576a022b732b14268507b05ebbbd3a746e3c2
isthistechsupport <diegorivero12262@gmail.com>
2023-01-02 12:28:40 -0500

    added docker buildx build command

---
+++ docker-build.sh
@@ -0,0 +1,8 @@
+docker buildx build \
+    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/386 \
+    -t isthistechsupport/tootbot:latest \
+    -t isthistechsupport/tootbot:3 \
+    -t isthistechsupport/tootbot:3.0 \
+    -t isthistechsupport/tootbot:3.0.0 \
+    --push \
+    -f ./Dockerfile .
