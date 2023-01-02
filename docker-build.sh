f599a1f9ac185e201780ac03865e6df125ff6c93
isthistechsupport <diegorivero12262@gmail.com>
2023-01-02 13:45:36 -0500

    increased patch number

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,6 @@ docker buildx build \
     -t isthistechsupport/tootbot:latest \
     -t isthistechsupport/tootbot:3 \
     -t isthistechsupport/tootbot:3.0 \
-    -t isthistechsupport/tootbot:3.0.0 \
+    -t isthistechsupport/tootbot:3.0.1 \
     --push \
     -f ./Dockerfile .
