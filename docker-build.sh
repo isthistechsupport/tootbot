334b906b84366d4640c9216d4cdafc96c9a6149d
Diego Rivero <32936232+isthistechsupport@users.noreply.github.com>
2024-09-30 17:01:47 -0500

    added docker build & push command

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,7 @@
-docker build --no-cache .
+docker build --no-cache \
+    -t isthistechsupport/tootbot:latest \
+    -t isthistechsupport/tootbot:3 \
+    -t isthistechsupport/tootbot:3.1 \
+    -t isthistechsupport/tootbot:3.1.2 . && \
+    docker image prune -f && \
+    docker push -a isthistechsupport/tootbot
