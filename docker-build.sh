542476c51715f94d6db0498480188caba7d2f42e
Diego Rivero <32936232+isthistechsupport@users.noreply.github.com>
2024-09-30 17:04:00 -0500

    fixed typo

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,5 @@ docker build --no-cache \
     -t isthistechsupport/tootbot:3 \
     -t isthistechsupport/tootbot:3.1 \
     -t isthistechsupport/tootbot:3.1.2 . && \
-    docker image prune -f && \
-    docker push -a isthistechsupport/tootbot
+docker image prune -f && \
+docker push -a isthistechsupport/tootbot
